8a60e076944dc5f208f31c8234d8021ab222d0e0
Edson <edsoneliassantana@gmail.com>
2026-09-24 20:33:04 -0300

    new

--- src/main/java/br/edu/ifpe/oxefood/api/cliente/ClienteService.java
+++ src/main/java/br/edu/ifpe/oxefood/api/cliente/ClienteService.java
@@ -17,7 +17,18 @@ public class ClienteService {
 
     public Cliente build(ClienteDTO dto) {
 
-        Cliente cliente = new Cliente();
+        Cliente cliente = null;
+
+      if (dto.getId() == null) { //Montado para o cadastro
+
+         cliente = new Cliente();
+
+      } else { //Consultado para a alteração
+
+         cliente = repository.findById(dto.getId()).get();
+      }
+
+
         cliente.setNome(dto.getNome());
         cliente.setDataNascimento(dto.getDataNascimento());
         cliente.setCpf(dto.getCpf());
@@ -43,5 +54,13 @@ public class ClienteService {
 
         return repository.findById(id).get(); //SELECT * FROM Cliente WHERE id = ?
     }
+
+    @Transactional
+    public Cliente atualizar(ClienteDTO dto) {
+
+    Cliente cliente = build(dto);
+    return repository.save(cliente);
+    }
+
 
 }
--- src/main/java/br/edu/ifpe/oxefood/api/produto/Produto.java
+++ src/main/java/br/edu/ifpe/oxefood/api/produto/Produto.java
@@ -1,5 +1,58 @@
-package br.edu.ifpe.oxefood.api.produto;
+package br.edu.ifpe.oxefood.api.cliente;
 
-public class Produto {
-    
+import java.time.LocalDate;
+
+import org.hibernate.annotations.SQLRestriction;
+
+import br.edu.ifpe.oxefood.util.EntidadeAuditavel;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+//JPA {
+@Entity
+@Table(name = "Produto")
+@SQLRestriction("habilitado = true")
+// }
+
+//lombok{
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+// }
+
+public class Produto extends EntidadeAuditavel  {
+  
+   @ManyToOne
+private Empresa empresa;
+
+@ManyToOne
+private CategoriaProduto categoria;
+
+@Column
+private String codigo;
+
+@Column
+private String titulo;
+
+@Column
+private String descricao;
+
+@Column
+private Double valorUnitario;
+
+@Column
+private Integer tempoEntregaMinimo;
+
+@Column
+private Integer tempoEntregaMaximo;
+
 }
+
+
